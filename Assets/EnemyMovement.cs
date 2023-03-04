4a335d87322cb5fd2bde39b9c8f359651e0eb817
Duck1712 <matej.25medakovic@gmail.com>
2023-03-04 19:25:25 +0100

    Pathfinding

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    NavMeshAgent enemy;
+    private Vector3 target;
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+        enemy.updateRotation = false;
+        enemy.updateUpAxis = false;
+    }
+
+    void Update()
+    {
+        SetTargetPosition();
+        SetEnemyPosition();
+    }
+    void SetTargetPosition()
+    {
+        target = player.position;
+    }
+    void SetEnemyPosition()
+    {
+        enemy.SetDestination(new Vector3(target.x,target.y));
+    }
+}
