cdcc28c49e99ac80164118c2928a8069a5b0231b
LemonsEarth <bonixsrbija@gmail.com>
2023-03-05 15:35:42 +0100

    more bonus level stuff

--- Assets/Scripts/Miscelaneous/DashExit.cs
+++ Assets/Scripts/Miscelaneous/DashExit.cs
@@ -8,10 +8,18 @@ public class DashExit : MonoBehaviour
     public SpriteRenderer whitescreen;
     public SpriteRenderer sword;
     public static int enemiesHit;
+    [SerializeField] GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
     private void OnTriggerEnter2D()
     {
         PlayerScore.score += 2000;
         StartCoroutine(KillBoss());
+        player.GetComponent<DashMove>().enabled = false;
+        player.transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
     }
 
     IEnumerator KillBoss()
